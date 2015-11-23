2a05a50d2e13bb364307bf58d372f463aab483bc
Sn0wFox <ruben.pericas@laposte.net>
2015-11-23 17:31:55 +0100

    Création projet

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+struct EtatNoeud
+{
+	int Occurences;
+	bool EstIsole;
+	EtatNoeud() : Occurences(0), EstIsole(true) { };
+	EtatNoeud(const EtatNoeud& unEtat) :	Occurences(unEtat.Occurences),
+											EstIsole(unEtat.EstIsole){ };
+	EtatNoeud& operator++() { ++Occurences; return *this; };	// prefix
+	EtatNoeud operator++(int) { EtatNoeud res(*this);			// postfix
+								++(*this);
+								return res; };
+};
+
+
+typedef map<string, EtatNoeud> DicoNoeuds;
+typedef map<string, int> DicoRequetes;
+
+
+int main(int argc, char** argv)
+{
+	// Si la clef n'existe pas, la valeur prend une valeur nulle (fonction du type de la clef)
+	DicoNoeuds ryan;
+	ryan["hi"]++;
+	ryan["hi2"]++;
+	ryan["hi"]++;	// Recherche dans une arbre rouge et noir... log(n)
+	
+	EtatNoeud e;
+
+	cout << e++.Occurences << endl;
+	cout << ++e.Occurences << endl;
+
+	for (DicoNoeuds::iterator it = ryan.begin(); it != ryan.end(); it++)
+	{
+		cout << it->first << ends << it->second.Occurences << endl;
+	}
+		
+
+	return 0;
+
+}
